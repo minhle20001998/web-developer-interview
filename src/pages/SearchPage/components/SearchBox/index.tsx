57fcd788300efbcf991c8399e5815af11f9ab63c
haphuong96 <phuongha96@gmail.com>
2024-10-13 20:48:02 +0700

    feat(task#2a): Typeahead Suggestion Dropdown

--- src/pages/SearchPage/components/SearchBox/index.tsx
+++ src/pages/SearchPage/components/SearchBox/index.tsx
@@ -1,6 +1,8 @@
 import SearchIcon from "@/components/icons/SearchIcon";
 import CrossIcon from "@/components/icons/CrossIcon";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { fetchSearchSuggestion } from "@/services/search";
+import debounce from "lodash/debounce";
 
 interface IProps {
   onSearch: (keyword: string) => void;
@@ -8,8 +10,9 @@ interface IProps {
 
 function SearchBox(props: IProps) {
   const { onSearch } = props;
-  const [suggestions] = useState<string[]>();
+  const [suggestions, setSuggestions] = useState<string[]>();
   const [searchInput, setSearchInput] = useState<string>();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -18,25 +21,56 @@ function SearchBox(props: IProps) {
     }
   };
 
+  const debouncedInput = useMemo(
+    () =>
+      debounce((searchInput) => {
+        fetchSearchSuggestion(searchInput).then(({ error, data }) => {
+          if (error) {
+            console.error(error);
+          } else if (data) {
+            setSuggestions(data.suggestions);
+            if (data.suggestions.length) {
+              setIsDropdownOpen(true);
+            }
+          }
+        });
+      }, 300),
+    []
+  );
+
+  useEffect(() => {
+    if (searchInput && searchInput?.length > 2) {
+      debouncedInput(searchInput);
+    } else {
+      debouncedInput.cancel();
+      if (suggestions?.length) setSuggestions([]);
+      if (isDropdownOpen) setIsDropdownOpen(false);
+    }
+  }, [searchInput, debouncedInput, suggestions?.length, isDropdownOpen]);
+
   return (
     <form
       className={
-        "flex rounded-lg border focus-within:border-primary-blue focus-within:rounded-bl-none" 
+        "flex rounded-lg border-2 focus-within:border-primary-blue" +
+        (isDropdownOpen ? " rounded-bl-none" : "")
       }
       onSubmit={handleSubmit}
     >
       <div className="flex-grow relative">
         <input
           type="text"
-          className="w-full h-full rounded-lg  focus:rounded-b-none focus:outline-none focus:ring-0 pl-4 pr-10"
+          className={
+            "w-full h-full rounded-lg focus:outline-none focus:ring-0 pl-4 pr-10" +
+            (isDropdownOpen ? " rounded-b-none" : "")
+          }
           onChange={(e) => setSearchInput(e.target.value)}
           aria-label="search-textfield"
         />
         <div className="absolute top-2/4 right-2 -translate-y-2/4">
           <CrossIcon />
         </div>
-        {suggestions && (
-          <div className="absolute w-full p-5 rounded-b-lg shadow-general border border-t-0 bg-white flex flex-col gap-[10px]">
+        {suggestions && isDropdownOpen && (
+          <div className="absolute w-full p-5 rounded-b-lg shadow-general border-x border-b translate-z bg-white flex flex-col gap-[10px] -z-10">
             {suggestions.map((suggestion) => (
               <div>{suggestion}</div>
             ))}
--- src/services/search.ts
+++ src/services/search.ts
@@ -1,4 +1,4 @@
-import { SearchResultResponse } from "@/types";
+import { SearchResultResponse, SearchSuggestionResponse } from "@/types";
 
 export const fetchSearchResult = async (_keyword: string) => {
   //TODO Try catch, validate json response
@@ -8,6 +8,19 @@ export const fetchSearchResult = async (_keyword: string) => {
     );
 
     const json: SearchResultResponse = await response.json();
+    return { error: null, data: json };
+  } catch (error) {
+    return { error, data: null };
+  }
+};
+
+export const fetchSearchSuggestion = async (_keyword: string) => {
+  try {
+    const response = await fetch(
+      "https://gist.githubusercontent.com/yuhong90/b5544baebde4bfe9fe2d12e8e5502cbf/raw/e026dab444155edf2f52122aefbb80347c68de86/suggestion.json"
+    );
+
+    const json: SearchSuggestionResponse = await response.json();
     return { error: null, data: json };
   } catch (error) {
     return { error, data: null };
